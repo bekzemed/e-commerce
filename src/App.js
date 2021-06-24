e9c8892c498750bee6f3a4552197e2048123e7ce
bekzemed <bereketzemed@gmail.com>
2021-06-24 13:50:39 +0300

    implemented firebase utils, included ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -5,26 +5,48 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import React from 'react';
-import {auth} from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // open subscription 
+    // open subscription
     // app listen to state changed on firebase auth
-   this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-    this.setState({currentUser: user});
-    console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({currentUser: user});
+      // console.log(user);
+
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // fetching the data stored in db
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+          // setState is async so its not guaranted to call it after setState what we can do is pass log into second argument of setState
+          // console.log(snapshot.data());
+
+          console.log(this.state);
+        });
+      }
+      // if user is null
+      else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -35,7 +57,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      <Header currentUser={this.state.currentUser} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
