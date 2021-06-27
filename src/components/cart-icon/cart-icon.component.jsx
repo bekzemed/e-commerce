5a98837fe8a198cf54035ef82b4a207465fe49d0
bekzemed <bereketzemed@gmail.com>
2021-06-27 21:03:59 +0300

    updating our app with reselect, replaced cart selectors with memoized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingCart } from '../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingCart className="shopping-icon" />
-    <span className="item-count">{cartItems.length}</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+// mapStateToProps called whenever any state change in the application, there is drawback of this which is, it will make component to be re-rendered
+// this can be prevented by applying MEMORIZATION in redux using RESELECT library
+const mapStateToProps = state => ({
+  // its just called selector which only select quantity from cartItems
+  itemCount: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = dispatch => ({
