62cac1ec8153468dd7d39655689fcd6b637ec2ce
bekzemed <bereketzemed@gmail.com>
2021-06-30 15:56:38 +0300

    moved our directory and shop data into reducer, created respective selectors and update corresponding component with new redux flow for directory/shop, normalize our shop data array

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+const CollectionOverview = ({ collections }) => (
+  <div className="collection-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectShopCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+);
+
+export const selectShopCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  collections => Object.keys(collections).map(keys => collections[keys])
+);
+
+export const selectShopCollection = collectionUrlParam =>
+  createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
+  );
