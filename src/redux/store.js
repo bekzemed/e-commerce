d3a4b51d04673822aab95f7823932030557772fa
bekzemed <bereketzemed@gmail.com>
2021-06-30 09:41:19 +0300

    redux persist added to persist cart reducer onto local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './root.reducer';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+const allData = {
+  store,
+  persistor,
+};
+
+export default allData;
