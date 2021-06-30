622a5038fe3d5a0123c349347109e0d2f094e667
bekzemed <bereketzemed@gmail.com>
2021-06-30 19:52:12 +0300

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51J85piDoa4IfqRlGQPLnqwZ51LjiOG3mlzfjEoKYZlwLHw10DdrwIAqQQ5jDaCXKepnM5WiJT8NjPaHgwxIY962T00kJ0ZYDnd';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-commerce"
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
